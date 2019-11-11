ab5eb6b3779416a4c1ce396f037015b47caccac7
jjia5435 <jjia5435@uni.sydney.edu.au>
2019-11-11 20:53:01 +1100

    initial comment

---
+++ LP script.js
@@ -0,0 +1,15 @@
+/* var signupChange = document.getElementById('signupButton');
+var loginChange = document.getElementById('loginButton');
+var move = document.getElementById('change');
+ */
+
+function moveToSignup() {
+    document.getElementById("change").style.transform = "translateX(90%)";
+}
+
+function moveToLogin() {
+    document.getElementById("change").style.transform = "translateX(0%)";
+}
+
+/* 
+loginChange.addEventListener("click", moveToSignup); */
